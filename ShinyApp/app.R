5ecae6c8f9f1b282b4e3a55cd10cf9fcde335919
Michael R. Hunsaker <hunsakerconsulting@gmail.com>
2024-06-28 12:40:35 -0600

    Update app.R

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -1,3 +1,20 @@
+#    Mainstreaming INCLUDE Algorithm Shiny App
+#    Copyright (C) 2024, Michael Ryan Hunsaker, M.Ed., Ph.D.
+#
+#   This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#########################################################################
+
 packages <- c("shiny", "shinyjs", "gplots", "RColorBrewer", "rpart", "rpart.plot", "e1071", "gridExtra", "shinythemes")
 install_if_missing <- function(packages) {
   # Check if each package is installed, install if not
